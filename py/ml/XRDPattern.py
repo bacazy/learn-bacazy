9445baaee47f54beb68dfbed9ae36e85e5d9e2fd
ZhangCheng <gc_zhangcheng@126.com>
2017-12-07 19:55:10 +0800

    modify XRDPattern.py

--- py/ml/XRDPattern.py
+++ py/ml/XRDPattern.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
-
 from matplotlib import pyplot as plt
-from scipy.signal import bsplines, cubic
 
 
 class XRDPattern:
@@ -10,18 +8,16 @@ class XRDPattern:
     XRD pattern draw class
     """
 
-    def __init__(self):
-        self.label = ''
+    def __init__(self, name, filename):
+        self.name = name
         self.peaks = []
         self.theta = []
         self.I = []
+        self.__load(filename)
 
-    def load(self, filename):
+    def __load(self, filename):
         xrdfile = open(filename)
         lines = xrdfile.readlines()[32:-1]
-        self.__readData(lines)
-
-    def __readData(self, lines):
         for xy in lines:
             x = re.findall(r'\d+\.\d+|\d+', xy)
             self.theta.append(float(x[0]))
@@ -30,12 +26,11 @@ class XRDPattern:
     def filter(self, method):
         pass
 
-    def plot(self):
+    def plotPeaks(self):
         plt.plot(self.theta, self.I)
-        plt.show()
 
 
 if __name__ == '__main__':
-    pattern = XRDPattern()
-    pattern.load('C:/workspace/laji/expriment/raw/XRD/20170527/2017052701/1.txt')
-    pattern.plot()
+    pattern = XRDPattern('xts','E:/laji/expriment/raw/XRD/20170527/2017052701/1.txt')
+    pattern.plotPeaks()
+    plt.show()
